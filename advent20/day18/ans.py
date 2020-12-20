abfa2b4175172a2274abf7e2c0a79a714af61191
TJ Yang <teejayyang35@gmail.com>
2020-12-19 19:06:33 -0500

    Add aoc day 18 part 2

--- advent20/day18/ans.py
+++ advent20/day18/ans.py
@@ -1,4 +1,6 @@
 from operator import add, mul
+from functools import reduce
+
 with open('input') as file:
     expressions = [line.strip() for line in file]
 
@@ -38,5 +40,43 @@ def parse1(expr, idx):
     return ans, idx
 
 
+def parse2(expr, idx):
+    ans = []
+
+    last_sum = None
+
+    while idx < len(expr):
+        c = expr[idx]
+        if c == ')':
+            break
+        elif c == ' ':
+            pass
+        elif c == '+':
+            pass
+        elif c.isnumeric():
+            c = int(c)
+            if last_sum is None:
+                last_sum = c
+            else:
+                last_sum += c
+        elif c == '*':
+            ans.append(last_sum)
+            last_sum = None
+        elif c == '(':
+            sub_ans, idx = parse2(expr, idx + 1)
+            if last_sum is None:
+                last_sum = sub_ans
+            else:
+                last_sum += sub_ans
+        else:
+            raise Exception(f'Cannot parse {c}')
+        idx += 1
+    ans.append(last_sum)
+    return reduce(mul, ans), idx
+
+
 # part 1
-print(sum(parse1(expr, 0)[0] for expr in expressions))
+# print(sum(parse1(expr, 0)[0] for expr in expressions))
+
+# part 2
+print(sum(parse2(expr, 0)[0] for expr in expressions))
